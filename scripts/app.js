5ed29dcb3c1451c8ec4c2c304bc02390bbf507a6
Aishwarya554 <aishwaryapoojary885@gmail.com>
2025-07-03 14:10:32 +0530

    Add hero section and make it responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
-console.log("E-Commerce Website Loaded");s
+console.log("Script loaded!"); // Add this line
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
